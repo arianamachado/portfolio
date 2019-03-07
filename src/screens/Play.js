d77639644e73fe347de12453a64e108858d9d782
Ariana Machado <ariana.machado@hotmail.ca>
2019-03-06 19:36:50 -0500

    Masonry working

--- src/screens/Play.js
+++ src/screens/Play.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Nav from '../components/Nav';
+import Masonry from 'react-masonry-component';
 
 const contentful = require('contentful');
 const client = contentful.createClient({
@@ -7,6 +8,10 @@ const client = contentful.createClient({
     accessToken: 'b525b139f2770ef4271f4ccc34f25157046984642a87138b55c1ac003d1fa4dc',
     environment: 'master'
 });
+
+const masonryOptions = {
+    transitionDuration: 0
+};
 
 class Play extends Component {
 
@@ -23,11 +28,40 @@ class Play extends Component {
     }
 
     render() {
-        // console.log(play)
+
+        // {
+        //     const childElements = [];
+
+        //     this.state.play.length &&
+        //         this.state.play.map((item, key) => {
+        //             childElements.push(
+        //                 <li className="image-element-class">
+        //                     <img src={item.fields.image.fields.file.url} />
+        //                 </li>
+        //             )
+        //         });
+
+        // };
+
         return (
             < main >
                 <h1 id="pageTitle">Play</h1>
+                <Masonry
+                    className={'my-gallery-class'} // default ''
+                    elementType={'ul'} // default 'div'
+                    options={masonryOptions} // default {}
+                    disableImagesLoaded={false} // default false
+                    updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                >
+                    {this.state.play.length &&
 
+                        this.state.play.map((item, key) => {
+                            return (
+                                <li className="image-element-class">
+                                    <img src={item.fields.image.fields.file.url} />
+                                </li>)
+                        })};
+                </Masonry>
             </main >
         );
     }
